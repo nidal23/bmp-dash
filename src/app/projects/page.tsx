c561d672d76cbf6b6461d483c54004a6cfbaea0b
nidal-intelo <nidal@intelo.ai>
2024-03-02 12:35:51 +0530

    -added project list -props passing to be fixed -project details to be filled in

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { Project } from '@/components/helper/projectList';
 
-const ProjectsPage = () => {
-  return (
-    <div className=' py-8'>ProjectsPage</div>
-  )
+interface ProjectProps {
+  projectList: Project[];
 }
 
-export default ProjectsPage
+const ProjectsPage: React.FC<ProjectProps> = ({ projectList }: ProjectProps) => {
+  return (
+    <div>
+      {projectList ? (projectList.map((project) => (
+        <main key={project.id} className='max-w-5xl py-10 px3 flex flex-col'>
+          <h2 className='font-bold text-lg tracking-tight'>
+            {project.name}
+          </h2>
+        </main>
+      ))):(<p>No Projects to display</p>)}
+    </div>
+  );
+}
+
+export default ProjectsPage;
