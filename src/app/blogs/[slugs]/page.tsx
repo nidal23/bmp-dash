e4480343a40bcd12a72d51d17f3b9c9c0f25335d
nidal-intelo <nidal@intelo.ai>
2024-04-07 21:16:26 +0530

    removed slug dummy date

--- src/app/blogs/[slugs]/page.tsx
+++ src/app/blogs/[slugs]/page.tsx
@@ -7,18 +7,18 @@ import { blogs } from '@/helpers/BlogStorage/blogStorage';
 const SingleBlogPage = () => {
   const router = useParams();
 
-  const { slugs } = router;
+  // const { slugs } = router;
 
 
   // Find the blog based on the slug
-  const blog = blogs.find((blog) => blog.index === Number(slugs));
+  // const blog = blogs.find((blog) => blog.index === Number(slugs));
 
-  if (!blog) notFound();
+  if (true) notFound();
 
   return (
     <div className='px-2 flex flex-col justify-center gap-8 items-center'>
-      <div className='font-bold text-3xl'>{blog.title}</div>
-      <div className='border p-4 rounded-md w-6/12'>{blog.content}</div>
+      {/* <div className='font-bold text-3xl'>{blog.title}</div>
+      <div className='border p-4 rounded-md w-6/12'>{blog.content}</div> */}
     </div>
   );
 };
