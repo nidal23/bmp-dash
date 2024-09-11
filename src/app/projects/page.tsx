8ccb664191a39c367702ca6eea8450e0799397c9
nidal-intelo <nidal@intelo.ai>
2024-09-12 00:49:08 +0530

    theme change: theme changed from black and hwite to black and yellow kodak

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -7,9 +7,9 @@ const ProjectsPage = () => {
   return (
     <div className='flex justify-center'>
       <div className='grid grid-cols-1 md:grid-cols-2 gap-4 m-auto lg:max-w-screen-lg'>
-        {projectList.length > 0 ? (
+        {projectList?.length > 0 ? (
           projectList.map((project) => (
-            <div key={project.id} className='border border-white rounded-lg hover:border-slate-500'>
+            <div key={project.id} className='border border-customYellow rounded-lg hover:border-yellow-400'>
               <ProjectLayout key={project.id} project={project}/>
             </div>
           ))
